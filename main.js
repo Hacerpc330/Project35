48e6149e83843df1fcfcf29f55bed3a05124138c
Hacerpc330 <hacerpc330@gmail.com>
2022-02-10 22:23:41 +0200

    Project35

---
+++ main.js
@@ -0,0 +1,148 @@
+// FoR 1
+// let k = +prompt('Enter K')
+// let n = prompt('How much times?')
+// for(let a = 1; a<=n ; a++){
+//     console.log(k)
+// }
+
+// FoR 2
+// let a = +prompt('First number')
+// let b = +prompt('Second number')
+// let n = (b-a)+1
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         console.log(i)
+//     }
+//     console.log(`How much times = ${n}`)
+// }
+//  else{
+//     alert('Error')
+// }
+
+// FoR 3
+// let a = +prompt('First number')
+// let b = +prompt('Second number')
+// let n = (b-a)-1
+// if (a<b){
+//     for(let i = a+1; i<b; i++){
+//         console.log(i)
+//     }
+//     console.log(`Numbers between numbers = ${n}`)
+// }
+//  else{
+//     alert('Ошибка')
+// }
+
+// FoR 4
+// let c = +prompt('How much for 1kg')
+// for(let a = 1; a<=10; a++){
+//     console.log(`Price for ${a} kg = ${c*a} $`)
+//     console.log('-------------------------')
+// }
+// FoR 5
+// let c = +prompt('How much for 1kg')
+// for(let a = 0.1; a<=1; a+=0.1){
+//     let s = c*a
+//     console.log(`Price for ${a.toFixed(1)} kg = ${Math.round(s)} $`)
+//     console.log('-------------------------')
+// }
+// FoR 6
+// let c = +prompt('Введите цену за 1кг')
+// for(let a = 1.2; a<=2; a+=0.2){
+//     let s = c*a
+//     console.log(`Price for ${a.toFixed(1)} kg = ${Math.round(s)} $`)
+//     console.log('-------------------------')
+// }
+// FoR 7
+// let a = +prompt('First number')
+// let b = +prompt('Second number')
+// k=0
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         k+=i   
+//     } console.log(k)
+// }
+//  else{
+//     alert('Error')
+// }
+
+// FoR 8 
+// let a = +prompt('First number')
+// let b = +prompt('Second number')
+// k=1
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         k*=i   
+//     } console.log(k)
+// }
+//  else{
+//     alert('Error')
+// }
+
+// FoR 9 
+// let a = +prompt('First number')
+// let b = +prompt('Second number')
+// k=0
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         k+=i**2        
+//     } console.log(k)
+// }
+//  else{
+//     alert('Error')
+// }
+
+// FoR 10
+// let n = +prompt('First number')
+// let k = 0
+// for (let a = 1; a<=n; a++){
+//     k+=(1/a)
+// } console.log(`Summa = ${k}`)
+
+// FoR 11 
+// let n = +prompt('First number')
+// let k = 0
+// for (let a = 0; a<=n; a++){
+//     k+=(n+a)**2
+// } console.log(`Summa = ${k}`)
+// FOR 12
+// let n = +prompt('First number')
+// c = 1
+// let k = 1
+// for (let a = 1.1; c<=n; a+=0.1){
+//     k*=a
+//     c++
+// } console.log(` Plus = ${k.toFixed(1)}`)
+
+// FoR 13
+// let n = +prompt('First number')
+// c = 1
+// let k = 0
+// for (let a = 1.1; c<=n; a+=0.2){
+//     k+= a - (a + 0.1)
+//     c++
+// } console.log(`Summa = ${k.toFixed(1)}`)
+
+// FoR 14
+// let n = +prompt('First number')
+// let c = (2*n)-1
+// let k = 0
+// for(let a = 1; a<=c ; a+=2){
+//    k+=a
+// }console.log('Summa = ' + k)
+
+// FoR 15
+// let a = +prompt('First number')
+// let n = +prompt('Second number')
+// for(n;n<0;n=n){
+// } console.log('A in N = ' + a**n)
+
+// FoR 16
+// let a = +prompt('First number')
+// let n = +prompt('Second number')
+// k = 1
+// for(i=1;i<=n;i++){
+//     k = a**i
+//     console.log('A in N = ' + k)
+//     console.log('------')
+// }
